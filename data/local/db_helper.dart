905f8729f850049e65d956799ae66e20900a8315
Paras Pithadiya <paraspithadiya10@gmail.com>
2025-02-19 18:33:59 +0530

    add sqflite

---
+++ data/local/db_helper.dart
@@ -0,0 +1,11 @@
+class DbHelper {
+  /// Singleton
+  DbHelper._();
+  static final DbHelper getInstance = DbHelper._();
+
+  Database? myDB;
+
+  /// db Open(path -> if exist -> open, if not exist -> create)
+
+  /// all queries
+}
