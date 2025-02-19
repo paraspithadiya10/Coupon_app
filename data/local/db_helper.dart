39e4e8164d112f22df98b29ec720b7ffb44337d8
Paras Pithadiya <paraspithadiya10@gmail.com>
2025-02-19 18:51:15 +0530

    add path_provider and path package

--- data/local/db_helper.dart
+++ data/local/db_helper.dart
@@ -1,11 +1,23 @@
+import 'package:sqflite/sqlite_api.dart';
+
 class DbHelper {
   /// Singleton
   DbHelper._();
   static final DbHelper getInstance = DbHelper._();
 
-  Data
+  Database? myDB;
 
   /// db Open(path -> if exist -> open, if not exist -> create)
+  Database getDB() {
+    if (myDB != null) {
+      return myDB!;
+    } else {
+      OpenDB();
+      return myDB!;
+    }
+  }
+
+  OpenDB() {}
 
   /// all queries
 }
