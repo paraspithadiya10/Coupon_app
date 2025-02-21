a053ea0e78deec3ab5a0e8c0ebbf6a17175c09aa
Paras Pithadiya <paraspithadiya10@gmail.com>
2025-02-21 19:19:26 +0530

    remove unnecessary packages

--- lib/data/local/db_helper.dart
+++ lib/data/local/db_helper.dart
@@ -4,7 +4,6 @@ import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:demo_app/screens/splash_screen.dart';
 
 class DbHelper {
   /// Singleton
